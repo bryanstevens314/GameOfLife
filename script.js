ddcedb26f61b687f7fa510332c6aaf4db8295de9
Henry Do <661034+vitaminh@users.noreply.github.com>
2018-11-01 15:02:42 -0700

    Exercises Complete

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let addRow = document.getElementById('add-row');
 let tableElement = document.getElementById("tableElement");
+let selectElement = document.getElementsByTagName('select')[0];
+let currentColor = 'red';
 
 function makeRow() {
   const tr = document.createElement('tr');
-  let columns = 3;
+  let columns = 20;
   while (columns >= 0) {
     const td = document.createElement('td');
     tr.append(td);
@@ -12,4 +14,17 @@ function makeRow() {
   tableElement.appendChild(tr);
 }
 
+function changeColor(cell) {
+
+}
+
 addRow.addEventListener("click", makeRow);
+tableElement.addEventListener("click", (event) => {
+  if (event.target.matches('td')) {
+    event.target.className = currentColor;
+  }
+});
+
+selectElement.addEventListener("change", (event) => {
+  currentColor = event.target.value;
+});
