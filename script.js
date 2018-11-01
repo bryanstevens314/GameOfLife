a9a6896f261a3d04f46ffe870fec34a5c438a561
Bryan Stevens <ryan_stevens314@yahoo.com>
2018-11-01 14:42:52 -0700

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// Your code here
+let addRow = document.getElementById('add-row');
+let tableElement = document.getElementById("tableElement");
+
+function makeRow() {
+  const tr = document.createElement('tr');
+  let columns = 3;
+  while (columns >= 0) {
+    const td = document.createElement('td');
+    tr.append(td);
+    columns--;
+  }
+  tableElement.appendChild(tr);
+}
+
+addRow.addEventListener("click", makeRow);
