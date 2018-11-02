5201795bb0468e045875b28e2a96f261e2d9447e
Duncan <duncangichimu@gmail.com>
2018-11-02 14:43:01 -0700

    calculate Neighbors func

--- script.js
+++ script.js
@@ -7,59 +7,51 @@ let bodyElement = document.getElementsByTagName('body')[0];
 let currentColor = 'red';
 let columns = 25;
 let rows = 50;
-window.onload = function () {
 
-  while (columns >= 0) {
-    columns--;
-    makeRow(columns);
+window.onload = function() {
+  let x = rows;
+  while (x >= 0) {
+    makeRow(x);
+    x--;
   }
 };
 
-function calculateNeighbors(element) {
-  let num = 8;
-  let leftTopCorner = 0;
-  let centerTop = 0;
-  let rightTopCorner = 0;
-  let centerRight = 0;
-  let rightBottomCorner = 0;
-  let bottomCenter = 0;
-  let leftBottomCorner = 0;
-  let centerleft = 0;
-  element.neighbors.push(
-    leftTopCorner,
-    centerTop,
-    rightTopCorner,
-    centerRight,
-    rightBottomCorner,
-    bottomCenter,
-    leftBottomCorner,
-    centerleft);
-
+function calculateNeighbors(x, y) {
+  return [
+    [x, y],
+    [x, y + 1],
+    [x + 1, y + 1],
+    [x + 1, y],
+    [x + 1, y - 1],
+    [x, y - 1],
+    [x - 1, y - 1],
+    [x, y - 1]
+  ].filter(pair => {
+    return pair[0] >= 0 && pair[0] <= rows && pair[1] >= 0 && pair[1] <= columns;
+  });
 }
 
-function makeRow() {
-
+function makeRow(x) {
   const tr = document.createElement('tr');
-  tr.obj = {
-    row: rows,
-    column: columns,
-    neighbors: []
-  };
-  calculateNeighbors(tr);
-  console.dir(tr.obj);
-  while (rows >= 0) {
+  let y = columns;
+
+  while (y >= 0) {
     const td = document.createElement('td');
+    const neighbors = calculateNeighbors(x, y);
+    td.obj = {
+      row: x,
+      column: y,
+      neighbors: neighbors
+    };
     tr.append(td);
-    rows--;
+    y--;
   }
   tableElement.appendChild(tr);
 }
 
-function startGame() {
-
-}
+function startGame() {}
 start.addEventListener('click', startGame);
-clear.addEventListener("click", () => {
+clear.addEventListener('click', () => {
   let tableItemsArray = Array.from(document.getElementsByTagName('td'));
   tableItemsArray.forEach(element => {
     element.className = 'lightGray';
