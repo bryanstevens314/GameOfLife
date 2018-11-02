916d434bb7573a257cc1a06bfdd63f48e8a44e3b
Bryan Stevens <ryan_stevens314@yahoo.com>
2018-11-02 14:16:54 -0700

    some more functionality

--- script.js
+++ script.js
@@ -1,21 +1,64 @@
 let addRow = document.getElementById('add-row');
 let clear = document.getElementById('clear');
+let start = document.getElementById('start');
 let tableElement = document.getElementById('tableElement');
 let selectElement = document.getElementsByTagName('select')[0];
 let bodyElement = document.getElementsByTagName('body')[0];
 let currentColor = 'red';
+let columns = 25;
+let rows = 50;
+window.onload = function () {
+
+  while (columns >= 0) {
+    columns--;
+    makeRow(columns);
+  }
+};
+
+function calculateNeighbors(element) {
+  let num = 8;
+  let leftTopCorner = 0;
+  let centerTop = 0;
+  let rightTopCorner = 0;
+  let centerRight = 0;
+  let rightBottomCorner = 0;
+  let bottomCenter = 0;
+  let leftBottomCorner = 0;
+  let centerleft = 0;
+  element.neighbors.push(
+    leftTopCorner,
+    centerTop,
+    rightTopCorner,
+    centerRight,
+    rightBottomCorner,
+    bottomCenter,
+    leftBottomCorner,
+    centerleft);
+
+}
 
 function makeRow() {
+
   const tr = document.createElement('tr');
-  let columns = 50;
-  while (columns >= 0) {
+  tr.obj = {
+    row: rows,
+    column: columns,
+    neighbors: []
+  };
+  calculateNeighbors(tr);
+  console.dir(tr.obj);
+  while (rows >= 0) {
     const td = document.createElement('td');
     tr.append(td);
-    columns--;
+    rows--;
   }
   tableElement.appendChild(tr);
 }
 
+function startGame() {
+
+}
+start.addEventListener('click', startGame);
 clear.addEventListener("click", () => {
   let tableItemsArray = Array.from(document.getElementsByTagName('td'));
   tableItemsArray.forEach(element => {
